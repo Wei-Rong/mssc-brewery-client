f64d1d50e9d3d1a8b2e5cd594aab032e08cabb3c
Wei Rong Low <wei.rong@human-e.co>
2021-08-19 21:32:19 +0800

    added client get,post and update methods as well as their corresponding tests

---
+++ src/test/java/guru/framework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,43 @@
+package guru.framework.msscbreweryclient.web.client;
+
+import guru.framework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer(){
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("NewBeer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer(){
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+}
